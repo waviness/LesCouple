70a1e86a29930b8c401e28b0e6b8f59ae37ffa2b
mengxiangyu-meng <52447504+mengxiangyu-meng@users.noreply.github.com>
2022-10-28 11:51:33 +0800

    Update image-edit.vue

--- pagesU/infor/image-edit.vue
+++ pagesU/infor/image-edit.vue
@@ -1,9 +1,36 @@
 <template>
-	<view class="">编辑头像</view>
+	<view class="upload_avatar_body">
+		<view class="upload_avatar" >
+			<u-upload
+				:fileList="imgList"
+				@afterRead="afterRead"
+				@delete="deletePic"
+				uploadIcon="plus"
+				:previewFullImage="true"
+			></u-upload>
+		</view>
+		<view class="upload_image">
+			<u--image src="https://cdn.uviewui.com/uview/album/1.jpg111" width="80px" height="80px" radius="2px" :lazy-load="true">
+				<view slot="error" style="font-size: 48rpx;"><u-icon name="photo"></u-icon></view>
+			</u--image>
+		</view>
+	</view>
 </template>
 
 <script>
+export default {
+	name: 'PersonalInfoAvatarEdit',
+	data() {
+		return{
+			imgList: []
+		}
+	}
+	
+}
 </script>
 
-<style>
+<style lang="scss">
+	.upload_avatar_body {
+		display: flex;
+	}
 </style>
